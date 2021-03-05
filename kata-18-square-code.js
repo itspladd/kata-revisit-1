58a043d47b150f20c5f61520434342a6b9eff65f
Paul Ladd <vlendle@gmail.com>
2021-03-05 00:54:13 +0000

    Added shell and test cases

---
+++ kata-18-square-code.js
@@ -0,0 +1,9 @@
+const squareCode = message => {
+  let result = message;
+  return result;
+};
+
+console.log(squareCode("chill out"));
+console.log(squareCode("feed the dog"));
+console.log(squareCode("have a nice day"));
+console.log(squareCode("if man was meant to stay on the ground god would have given us roots"));
